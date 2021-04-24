5b09e0120115ac55c073131b36ab6f6d84cbc3fb
Kill Waas <58008359+ChronoHomes@users.noreply.github.com>
2021-04-24 10:04:11 +0200

    added all constructors to StackEmptyException

--- src/A01_Stack/StackEmptyException.java
+++ src/A01_Stack/StackEmptyException.java
@@ -4,4 +4,22 @@ public class StackEmptyException extends Exception {
 
 	private static final long serialVersionUID = 52034036L;
 
+	public StackEmptyException() {
+	}
+
+	public StackEmptyException(String message) {
+		super(message);
+	}
+
+	public StackEmptyException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StackEmptyException(Throwable cause) {
+		super(cause);
+	}
+
+	public StackEmptyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
 }
