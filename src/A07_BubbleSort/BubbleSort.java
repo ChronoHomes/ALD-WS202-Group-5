7ec4a737fe81f5827386782be8ca46889af8d724
Thomas Gugatschka <thomas.gugatschka@gmail.com>
2021-05-08 22:55:37 +0200

    bubble initial

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -15,12 +15,12 @@ public class BubbleSort implements PersonenSort {
 		// TODO - add some sort of counter to reduce checks -> since in first loop most "right" element is already in place -> not need to check again
 		// TODO - add temp variable names so it is easier to read?
 
-		int counter = 0;
+		int counter = 1; // start with 1 for inital array offset and then reuse it as performance counter
 
 		while (changed){
 			changed = false;
 
-			for (int i = 0; i < (personen.length - 1 - counter); i++) {
+			for (int i = 0; i < (personen.length - counter); i++) {
 				if (personen[i].compareTo(personen[i+1]) > 0){
 					tmpPerson = personen[i];
 					personen[i] = personen[i+1];
