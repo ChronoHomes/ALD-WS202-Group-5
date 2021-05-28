5feb38c8f48f2b1e11453186604f9d1c48630341
Thomas Gugatschka <thomas.gugatschka@gmail.com>
2021-05-28 17:30:32 +0200

    updated - comment

--- src/A06_Tiefensuche/Tiefensuche.java
+++ src/A06_Tiefensuche/Tiefensuche.java
@@ -48,7 +48,6 @@ public class Tiefensuche extends BaseTree<Film> {
 		return getMinMaxPreOrder(min, max, root);	// call method recursive with min & max from method parameter and the root
 	}
 
-
 	public List<String> getMinMaxPreOrder(double min, double max, Node<Film> node) {	// overload method to call it recursive
 
 		List<String> list = new ArrayList<>();		// create new list to store film title
@@ -56,11 +55,14 @@ public class Tiefensuche extends BaseTree<Film> {
 		if (node == null)		// termination of recursion
 			return list;
 
-		if (node.getValue().getLänge() > min && node.getValue().getLänge() < max)	// check if duration is within min & max parameter
-			list.add(node.getValue().getTitel());									// if it is add to list
+		if (node.getValue().getLänge() > min && node.getValue().getLänge() < max)    // check if duration is within min & max parameter
+			list.add(node.getValue().getTitel());                                    // if it is add to list
 
-		list.addAll(getMinMaxPreOrder(min, max, node.getLeft()));					// call method recursive on left leave
-		list.addAll(getMinMaxPreOrder(min, max, node.getRight()));					// call method recursive on right leave
+			list.addAll(getMinMaxPreOrder(min, max, node.getLeft()));                    // call method recursive on left leave
+			list.addAll(getMinMaxPreOrder(min, max, node.getRight()));                   // call method recursive on right leave
+
+
+		//TODO: Optimierung -> for better performance check if "FILM" is within length
 
 		return list;
 	}
