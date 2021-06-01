479931a7de5aa22877cd470a9fc99433431250b9
Thomas Gugatschka <thomas.gugatschka@gmail.com>
2021-06-01 23:07:56 +0200

    added - comments

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort implements PersonenSort {
 	/**
 	 * Sortier-Funktion
 	 */
-	/** Big O notation -> O(n²) */
+	/** Big O notation -> O(n²) */ // O(n²/2) ??? exact // Best case O(n)
 	public void sort(Person[] personen) {
 
 		Person tmpPerson;			// temporary helper for sorting process
@@ -17,7 +17,7 @@ public class BubbleSort implements PersonenSort {
 			changed = false;		// set termination condition always to false, to terminate in case we do not enter IF
 
 			for (int i = 0; i < (personen.length - perfCount); i++) {	//
-				if (personen[i].compareTo(personen[i+1]) > 0){			// compare two elements which are next to each other and check if they must be exchanged
+				if (personen[i].compareTo(personen[i+1]) > 0){			// compare two elements which are next to each other and check if they must be exchanged // > 0 and not >= 0 to reduce operations
 					tmpPerson = personen[i];							// store element in helper variable
 					personen[i] = personen[i+1];						// overwrite element with next element next to it (smaller one)
 					personen[i+1] = tmpPerson;							// write on 2nd element the content of the helper
