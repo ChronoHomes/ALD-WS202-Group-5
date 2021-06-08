6258e17dee376d25b42b81ac8450be8382fd9213
Thomas Gugatschka <thomas.gugatschka@gmail.com>
2021-06-08 18:40:41 +0200

    initial - version with IF/ELSE for basic proof of function

--- src/A12_Zustandsautomat/CSVParser_1.java
+++ src/A12_Zustandsautomat/CSVParser_1.java
@@ -18,39 +18,50 @@ public class CSVParser_1 {
 
 		System.out.println("Input String: " + str);
 
-//		for (String line : str.split("\n|\r")) {
-//			if (!line.trim().isEmpty())
-//				System.out.println("Carriage return found:" + line.trim());
-//		}
-
 
 		//	System.out.println("str.length: " + str.length());
 		//	String[] split = str.split(SEPARATOR);
 		//	System.out.println("Arrays.toString(split): " + Arrays.toString(split));
 
+		CSVResult result = new CSVResult();
+
+
 
 		for (int i = 0; i < str.length(); i++) {
 
 			char c = str.charAt(i);
-			System.out.println((int)c);
+			int ascii = (int) c;
+			System.out.println("isTextData: " + isTextData(c));
+			System.out.println((int) c);
 
-			//System.out.println((int) SEPARATOR); // -> int 44 for seperator
 
-			//-> cast char to int and build on that the statemachine
 
-			// if length of string reached and last two elements are not 10 and 13
-			// -> we reached the end of the string (=last element)
+			if (ascii == 10 || ascii == 13){
+				System.out.println("do nothing"); // CRLF
+			} else if (ascii == 44){
+				result.addValue();
+			} else if (ascii == 34){ // "
+
+			} else {
+				result.appendChar(c);
+			}
 
 			switch(state) {
 
 			} // switch end
 		}
 
+		/*
+		//System.out.println((int) SEPARATOR); // -> int 44 for seperator
 
-		CSVResult result = new CSVResult();
-		result.appendChar('1');
-		result.appendChar('2');
-		result.appendChar('3');
+		//-> cast char to int and build on that the statemachine
+
+		// if length of string reached and last two elements are not 10 and 13
+		// -> we reached the end of the string (=last element)
+
+		 */
+
+
 		result.addValue();
 		return result;
 	}
